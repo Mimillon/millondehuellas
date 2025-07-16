00ff6406adbe9b5a0af54498dd31d2aacc2f3d33
Mimillon <candeluis2021@gmail.com>
2025-07-16 12:03:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+const TOTAL_PIXELES = 1000;
+const jsonURL = 'data.json';
+const formularioURL = 'https://docs.google.com/forms/d/e/1FAIpQLSdNsFaS88azYxKMneXh3MdrKQ2sCkR5XzxDh6mcXz9Z8VEK3g/viewform';
 
-fetch('data.json')
-  .then(response => response.json())
+fetch(jsonURL)
+  .then(r => r.json())
   .then(data => {
     const grid = document.getElementById('pixel-grid');
-    for (let i = 0; i < 100; i++) {
+    const ocupados = {};
+    data.forEach((entry, i) => {
+      ocupados[entry.posicion] = entry;
+    });
+
+    for (let i = 1; i <= TOTAL_PIXELES; i++) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
 
-      if (data[i]) {
-        pixel.title = `${data[i].nombre}: ${data[i].mensaje}`;
-        if (data[i].imagen) {
-          pixel.style.backgroundImage = `url(${data[i].imagen})`;
-          pixel.style.backgroundSize = 'cover';
-        }
-        pixel.onclick = () => window.open(data[i].link, '_blank');
+      if (ocupados[i]) {
+        pixel.title = `${ocupados[i].nombre}: ${ocupados[i].mensaje}`;
+        pixel.style.backgroundImage = `url(${ocupados[i].imagen})`;
+        pixel.style.backgroundSize = 'cover';
+        pixel.onclick = () => window.open(ocupados[i].link, '_blank');
+        pixel.classList.add("ocupado");
       } else {
-        pixel.onclick = abrirFormulario;
+        pixel.title = "Disponible - haz clic para reservar";
+        pixel.onclick = () => window.open(formularioURL + `?usp=pp_url&entry.1234567890=${i}`, '_blank');
       }
+
       grid.appendChild(pixel);
     }
-  })
-  .catch(err => console.error('Error al cargar datos:', err));
+  });
